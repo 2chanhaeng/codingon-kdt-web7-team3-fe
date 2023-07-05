a055adaf160ec72337bfad2ef6938662f5276435
Yerim96 <chzh9993@naver.com>
2023-07-05 09:55:24 +0900

    feat: api 요청 후 응답 profiles에 저장

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 
 export default function profiles() {
   const [profiles, setProfiles] = useState([] as Profile[]);
+  useEffect(() => {
+    axios.get("/api/profiles").then((response) => {
+      setProfiles(response.data);
+    });
+  });
   return (
     <main>
     </main>
