78812add3a68e724f164e111358dab619b416658
Yerim96 <chzh9993@naver.com>
2023-07-05 10:03:42 +0900

    feat: Profile 타입 생성
    API 에 요청해서 받아올 profile 의 타입

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+type Profile = { id: number; name: string; infomation: string };
+
 export default function profiles() {
   const [profiles, setProfiles] = useState([] as Profile[]);
   useEffect(() => {
