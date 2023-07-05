4a21207fc3ec59dca8f456f9836fab458dee6493
Yerim96 <chzh9993@naver.com>
2023-07-05 09:54:31 +0900

    feat: profiles 상태 생성

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function profiles() {
+  const [profiles, setProfiles] = useState([] as Profile[]);
   return (
     <main>
     </main>
