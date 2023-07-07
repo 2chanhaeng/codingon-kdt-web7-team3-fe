879809b3c6ca1eed9db7fca6c4e5e078f1157dc1
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-07 12:32:27 +0900

    feat: 채팅 페이지 정의

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,6 +1,17 @@
 "use client";
-import { useState } from "react";
+
+import React from "react";
+import { Rooms, Messages, Header } from "@/components/chats";
+import { ChatsContextProvider } from "@/contexts/chats";
 
 export default function ChatPage() {
-  return <main></main>;
+  return (
+    <main>
+      <ChatsContextProvider>
+        <Header />
+        <Rooms />
+        <Messages />
+      </ChatsContextProvider>
+    </main>
+  );
 }
