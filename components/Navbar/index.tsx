7f5f4c9404c673474298a366437ba1ce77b434b0
Yerim96 <chzh9993@naver.com>
2023-07-10 20:26:53 +0900

    test: navbar 잘 되는지 테스트

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+import style from "styles.module.scss";
+export default function Navbar() {
+  return (
+    //   navbar goes here
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  );
+}
