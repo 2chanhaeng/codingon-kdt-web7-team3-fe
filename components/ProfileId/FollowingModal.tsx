b3f662165d7df1824073ae94293a17cea45a0ada
Yerim96 <chzh9993@naver.com>
2023-07-10 19:16:13 +0900

    feat: props 타입지정

--- components/ProfileId/FollowingModal.tsx
+++ components/ProfileId/FollowingModal.tsx
@@ -1,4 +1,10 @@
 import { ProfileType } from "@/types/profile";
 import styles from "app/profile/styles.module.scss";
 export default function FollowingModal({
+  followingProfiles,
+  closeFollowingModal,
+}: {
+  followingProfiles: ProfileType[];
+  closeFollowingModal: () => void;
+}) {
 }
