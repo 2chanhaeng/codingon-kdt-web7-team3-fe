c6519010249d0c15ca8e2d3b60e6c7a3e9f6768d
Yerim96 <chzh9993@naver.com>
2023-07-10 19:05:31 +0900

    feat: tagModal의 props 타입지정

--- components/ProfileId/TagModal.tsx
+++ components/ProfileId/TagModal.tsx
@@ -1,4 +1,11 @@
 import { ProfileType } from "@/types/profile";
 import styles from "app/profile/styles.module.scss";
+
 export default function TagModal({
+  profileTags,
+  toggleTagModal,
+}: {
+  profileTags: ProfileType[];
+  toggleTagModal: () => void;
+}) {
 }
