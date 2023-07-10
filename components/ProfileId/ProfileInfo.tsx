eecee4913c8b5c9c0c1be783eafd1b1d6388d724
Yerim96 <chzh9993@naver.com>
2023-07-10 19:14:28 +0900

    feat: profileInfo props의 타입지정

--- components/ProfileId/ProfileInfo.tsx
+++ components/ProfileId/ProfileInfo.tsx
@@ -1,4 +1,10 @@
 import { ProfileType } from "@/types/profile";
 import styles from "app/profile/styles.module.scss";
 export default function ProfileInfo({
+  id,
+  profiles,
+}: {
+  id: string;
+  profiles: ProfileType[];
+}) {
 }
