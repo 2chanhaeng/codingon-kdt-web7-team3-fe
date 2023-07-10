5f62872c8fe567374b1c5ee0f51fab4fb5f7442a
Yerim96 <chzh9993@naver.com>
2023-07-10 19:18:44 +0900

    feat: FollowerModal props 타입지정

--- components/ProfileId/FollowerModel.tsx
+++ components/ProfileId/FollowerModel.tsx
@@ -2,4 +2,10 @@ import { ProfileType } from "@/types/profile";
 import styles from "app/profile/styles.module.scss";
 
 export default function FollowerModal({
+  followerProfiles,
+  closeFollowergModal,
+}: {
+  followerProfiles: ProfileType[];
+  closeFollowergModal: () => void;
+}) {
 }
