9efe1bc47329f8b3b3a5b7878e26d3d6dd855dcc
Yerim96 <chzh9993@naver.com>
2023-07-10 19:11:50 +0900

    feat: profilePosts의 배열 출력

--- components/ProfileId/ProfilePost.tsx
+++ components/ProfileId/ProfilePost.tsx
@@ -5,4 +5,14 @@ export default function ProfilePost({
 }: {
   profilePosts: ProfileType[];
 }) {
+  return (
+    <section>
+      {profilePosts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.name}</h3>
+          <p>{post.information}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
