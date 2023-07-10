9416239069bcf846cef1060ae73b87a7b13e2ddf
Yerim96 <chzh9993@naver.com>
2023-07-10 19:21:15 +0900

    feat: profile/[id] 페이지 임시변수 exampleProfiles

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,4 +1,12 @@
 export default function ProfileId({ params }: { params: number }) {
   console.log(params);
   return "profileid";
+const exampleProfiles = [
+  {
+    id: "1",
+    name: "김한솔",
+    information: "풋볼을 좋아함",
+  },
+  { id: "2", name: "김안나", information: "운동을 좋아함" },
+];
 }
