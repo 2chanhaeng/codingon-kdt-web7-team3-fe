ae7131b4541f6f549d845b01e472a3ac866cb5d2
Yerim96 <chzh9993@naver.com>
2023-07-10 19:16:57 +0900

    feat: 모달 내부코드. 팔로잉 리스트 보여주기

--- components/ProfileId/FollowingModal.tsx
+++ components/ProfileId/FollowingModal.tsx
@@ -7,4 +7,17 @@ export default function FollowingModal({
   followingProfiles: ProfileType[];
   closeFollowingModal: () => void;
 }) {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <h2>Following</h2>
+        <ul>
+          {followingProfiles.map((profile) => (
+            <li key={profile.id}>{profile.name}</li>
+          ))}
+        </ul>
+        <button onClick={closeFollowingModal}>닫기</button>
+      </div>
+    </div>
+  );
 }
