0b89748c748ab7f114920f2b776df5f71cbaab1f
Yerim96 <chzh9993@naver.com>
2023-07-10 20:13:23 +0900

    feat: AllProfileList props 타입지정

--- components/ProfileId/AllProfileList.tsx
+++ components/ProfileId/AllProfileList.tsx
@@ -1,5 +1,10 @@
 import { ProfileType } from "@/types/profile";
 import Link from "next/link";
 export default function AllProfileList({
+  profiles,
+}: {
+  profiles: ProfileType[];
+}) {
+  {
   }
 }
