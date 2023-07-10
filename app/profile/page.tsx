7b27df23e03147bca1a35b8544b43352b047e806
Yerim96 <chzh9993@naver.com>
2023-07-10 20:30:15 +0900

    feat: 비로그인 로직시 사용할 코드. 현재는 사용하지 않는 코드

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,11 +9,23 @@ import Navbar from "@/components/Navbar";
 
 export default function ProfilesPage() {
   const [profiles, setProfiles] = useState([] as ProfileType[]);
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     axios.get("/api/profiles").then((response) => {
       setProfiles(response.data);
     });
   });
+  //비로그인시 인기순으로 프로필 정렬
+  const reqProfiles = (e: Event) => {
+    const cursor = profiles.at(-1)?.id ?? "";
+    const famousApi = new URL("/api/profiles/famous", window.location.href);
+    famousApi.searchParams.append("cursor", cursor);
+    fetch(famousApi)
+      .then((res) => res.json())
+      .then(({ profiles: newProfiles }: { profiles: ProfileType[] }) =>
+        setProfiles((prof) => prof.concat(newProfiles))
+      );
+  };
   return (
     <main>
       <ul>
