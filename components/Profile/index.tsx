8b52b73acaf5f8fa7f15012c8c349473e5c7b358
Yerim96 <chzh9993@naver.com>
2023-07-10 21:00:07 +0900

    feat:ProfileComponentProps 타입

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import { ProfileType } from "@/types/profile";
+interface ProfileComponentProps {
+  profile: ProfileType;
+  handleProfileEdit: (profile: ProfileType) => void;
+}
 
 export default function ProfileComponent({
   profile: { id, name, infomation },
