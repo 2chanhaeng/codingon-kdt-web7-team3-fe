258d29f9033b0b9ff11652ada7cb3bea74ebec6a
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-11 13:32:29 +0900

    feat: form 상태 정의

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,56 +3,27 @@ import axios from "axios";
 import React, { useCallback, useState } from "react";
 
 export default function LoginPage() {
-  const [userId, setUserId] = useState("");
-  const [userPw, setUserPw] = useState("");
-  const [success, setResult] = useState("");
-
-  const handleLogin = async () => {
-    if (userId === "" || userPw === "") {
-      setResult("아이디와 비밀번호를 입력하세요");
-      localStorage.setItem("login", "true");
-      return;
-    }
-
-    try {
-      const response = await axios.post("http://localhost:8000/api/login", {
-        username: userId,
-        password: userPw,
-      });
 interface LoginForm {
   username: string;
   password: string;
 }
 
-      if (response.data.success === true) {
-        setResult("로그인에 성공하였습니다.");
-      } else {
-        setResult("로그인에 실패하였습니다.");
-      }
-    } catch (error) {
-      setResult("로그인 요청을 처리하는 동안 오류가 발생하였습니다.");
-    }
-  };
+  const [form, setForm] = useState({} as LoginForm);
+  const handleChange = onChange(setForm);
 
   return (
     <main>
-      <h4>로그인</h4>
-      <form>
-        <label htmlFor="id">아이디</label>
-        <input
-          type="text"
-          id="id"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-        />
+      <h1>로그인</h1>
+      <form onSubmit={handleRegister}>
+        <label>
+          아이디
+          <input type="text" name="username" onChange={handleChange} />
+        </label>
         <br />
-        <label htmlFor="pw">비밀번호</label>
-        <input
-          type="password"
-          id="pw"
-          value={userPw}
-          onChange={(e) => setUserPw(e.target.value)}
-        />
+        <label>
+          비밀번호
+          <input type="password" name="password" onChange={handleChange} />
+        </label>
         <br />
         <button type="button" onClick={handleLogin}>
           로그인
