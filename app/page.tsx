1c4c897f7b58bbcee515cace66ed962c36f1c54a
Yerim96 <chzh9993@naver.com>
2023-07-12 02:13:57 +0900

    feat: 메인 html 구조변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import style from "app/styles.module.scss";
 
 export default function Home() {
   return (
-    <nav>
-      메인
-      <Link href={"/login"}>로그인 </Link>
-      <Link href={"/signup"}>회원가입</Link>
-    </nav>
+    <main>
+      <Link href={"/login"}>
+        <button type="button">로그인</button>
+      </Link>
+
+      <button type="button">
+        <Link href={"/signup"}>회원가입</Link>
+      </button>
+    </main>
   );
 }
