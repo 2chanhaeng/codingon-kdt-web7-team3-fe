eba519ca094cc29b3a4b8fcbb636240be371b8a0
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-12 10:21:08 +0900

    refac: 회원가입 페이지 Material UI 적용

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,62 +1,133 @@
 "use client";
-import { useRouter } from "next/navigation";
+
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-interface SignupForm {
-  username: string;
-  password: string;
-}
-
-export default function Signup() {
-  const { replace } = useRouter();
-  const [form, setForm] = useState({} as SignupForm);
-  const handleRegister = onFormSubmit(form, replace);
-  const handleChange = onChange(setForm);
-
+function Copyright(props: any) {
   return (
-    <div>
-      <h4>회원가입</h4>
-      <form onSubmit={handleRegister}>
-        <label>
-          아이디
-          <input type="text" name="username" onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          비밀번호
-          <input type="password" name="password" onChange={handleChange} />
-        </label>
-        <br />
-        <button type="submit">회원가입</button>
-        <dialog onClick={(e) => e.currentTarget.close()} id="failed">
-          <p>회원가입 실패</p>
-        </dialog>
-      </form>
-    </div>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
 
-/** 회원가입 폼을 제출하는 함수 */
-function onFormSubmit(
-  form: SignupForm,
-  replace: (path: string) => void
-): React.FormEventHandler<HTMLFormElement> {
-  return async (e) => {
-    e.preventDefault();
+export default function SignUp() {
+  const { replace } = useRouter();
+  const [failed, setFailed] = useState(false);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const data = {
+      username: form.get("username"),
+      password: form.get("password"),
+    };
     const res = await fetch("/api/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form),
+      body: JSON.stringify(data),
     });
-    if (res.ok) replace("/login");
-    else document.getElementById("failed")?.show();
+    if (res.ok) {
+      const { access } = await res.json();
+      localStorage.setItem("access", access); // TODO: 쿠키로 바꾸기
+      replace("/");
+    } else {
+      console.log(res);
+      setFailed(true);
+    }
   };
-}
+  const handleClose = () => setFailed(false);
 
-/** signupForm의 키값을 name으로 받아서 value를 넣어줌 */
-function onChange(setForm: React.Dispatch<React.SetStateAction<SignupForm>>) {
-  return (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((form) => ({ ...form, [name]: value }));
-  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Dialog open={failed} onClose={handleClose}>
+            <DialogTitle>Signup failed</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                The Username is already taken.
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Login
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
+  );
 }
