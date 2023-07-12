083c848c255e488fbad5fe4fc144691f3bb1b9fd
Yerim96 <chzh9993@naver.com>
2023-07-12 21:23:33 +0900

    feat: 기존에 있던 로그인 회원가입 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,7 @@ export default function Home() {
 
   return (
     <main>
-      <Link href={"/login"}>
-        <button type="button">로그인</button>
-      </Link>
 
-      <button type="button">
-        <Link href={"/signup"}>회원가입</Link>
-      </button>
     </main>
   );
 }
