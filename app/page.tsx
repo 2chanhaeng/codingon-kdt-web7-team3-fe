71a4475bffeddc63ad0bfdf8c7f38c3fc5bee3a1
Yerim96 <chzh9993@naver.com>
2023-07-12 21:22:00 +0900

    feat: app/page meterial 사용 요소 import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
-import style from "app/styles.module.scss";
-
+import Header from "@/components/Main/Header";
+import MainFeaturedPost from "@/components/Main/MainFeaturedPost";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import style from "./styles.module.scss";
 export default function Home() {
   return (
     <main>
