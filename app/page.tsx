42ff356450dbba6fafe2494297099ed7ee0fe03d
Yerim96 <chzh9993@naver.com>
2023-07-12 21:22:52 +0900

    feat: MainFeaturedPost에 보낼 인자값 키와값으로 보내기

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import style from "./styles.module.scss";
 export default function Home() {
+  const mainFeaturedPost = {
+    title: "취미로 만나는 우리",
+    description: "취미 기반 커뮤니티",
+    image: "/mainimage.jpg",
+    imageText: "main image description",
+    linkText: "Continue reading…",
+  };
+
   return (
     <main>
       <Link href={"/login"}>
